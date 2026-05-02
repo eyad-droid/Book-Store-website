1603dccc226a5603c7f4c415108802434c3adf04
marawantarek18mt-eng <marawantarek18.mt@gmail.com>
2026-05-02 18:57:16 +0300

    final update of the contact page

---
+++ js/form.js
@@ -0,0 +1,23 @@
+// forms.js
+
+document.querySelector(".contact-form").addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("Lname").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let recommendations = document.getElementById("Recommendations").value;
+
+    localStorage.setItem("fname", fname);
+    localStorage.setItem("lname", lname);
+    localStorage.setItem("email", email);
+    localStorage.setItem("phone", phone);
+    localStorage.setItem("recommendations", recommendations);
+
+    alert("Data Saved Successfully");
+
+    document.querySelector(".contact-form").reset();
+
+});
